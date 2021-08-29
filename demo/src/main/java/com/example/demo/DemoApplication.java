36db40741211bbda6823ec6f4b83e2131f1f841a
Freddy Yuca <ymfreddy@gmail.com>
2021-08-29 00:57:33 -0400

    modificaicon 3

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class DemoApplication {
 	// modificacion
 	// modificacion 2
+	// modificacion 3
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
